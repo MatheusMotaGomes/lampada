7d5038449a60d9e6415ea4a60835ba12fb5ea335
Matheus Mota <matheusmgf2010@gmail.com>
2024-03-14 14:45:41 -0300

    lampada fica quebrada
    agora a lampada fica  quebrada

--- script.js
+++ script.js
@@ -2,20 +2,33 @@ const lampada = document.getElementById('lampada_ligada');
 const ligar = document.getElementById('ligar');
 const desligar = document.getElementById('desligar');
 
+function verificar(){
+  return lampada.src.indexOf('quebrada') > -1
+}
+
 function ligar_lampada(){
-  lampada.src = './img/ligada.jpg';
+  if( !verificar() ){
+      lampada.src = './img/ligada.jpg';
+  }
 }
 
 function desligar_lampada(){
-  lampada.src = './img/desligada.jpg'
+  if( !verificar() ){
+     lampada.src = './img/desligada.jpg'
+  }
 }
 
 function quebrar_lampada(){
-  lampada.src  = './img/quebrada.jpg'
+  lampada.src  = './img/quebrada.jpg';
+  verificador += 1;
+  console.log(verificador);
 }
 
-ligar.addEventListener('click' , ligar_lampada);
-desligar.addEventListener('click', desligar_lampada);
-lampada.addEventListener('mouseover' , ligar_lampada);
-lampada.addEventListener('mouseleave' , desligar_lampada);
-lampada.addEventListener('dblclick' , quebrar_lampada);
+
+
+  ligar.addEventListener('click' , ligar_lampada);
+  desligar.addEventListener('click', desligar_lampada);
+  lampada.addEventListener('mouseover' , ligar_lampada);
+  lampada.addEventListener('mouseleave' , desligar_lampada);
+  lampada.addEventListener('dblclick' , quebrar_lampada);
+ 
